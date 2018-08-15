57c44a4110f3327931b5af259fa32cacd60ad067
Vladimir Glinskikh <support@zhelezyaka.kz>
2018-08-15 11:15:02 +0600

    verified

---
+++ src/main/java/collections/QueuesAndStacks_2_16/Main.java
@@ -0,0 +1,14 @@
+package collections.QueuesAndStacks_2_16;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayDeque<Integer> queue = new ArrayDeque<>();
+        queue.add(2);
+        queue.add(0);
+        queue.add(1);
+        queue.add(7);
+        System.out.println(queue);
+    }
+}
