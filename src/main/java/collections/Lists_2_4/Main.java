8736e34964bdd629404f79af45b406866aaf2acc
Vladimir Glinskikh <support@zhelezyaka.kz>
2018-08-16 04:13:31 +0600

    verified

---
+++ src/main/java/collections/Lists_2_4/Main.java
@@ -0,0 +1,11 @@
+package collections.Lists_2_4;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>(Arrays.asList(2, 0, 1, 7));
+
+        System.out.println(list);
+    }
+}
