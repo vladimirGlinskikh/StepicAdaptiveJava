71abf181855b059195706741a8a32e3770cccdda
Vladimir Glinskikh <support@zhelezyaka.kz>
2018-08-16 04:05:45 +0600

    verified

---
+++ src/main/java/collections/Lists_2_7_Converting/Main.java
@@ -0,0 +1,37 @@
+package collections.Lists_2_7_Converting;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<GreekLetter> letterList = new ArrayList<>();
+        letterList.add(new GreekLetter("Gamma", 3));
+        letterList.add(new GreekLetter("Omega", 24));
+        letterList.add(new GreekLetter("Alpha", 1));
+
+        Object[] objArray = letterList.toArray();
+
+        for (int i = 0; i < objArray.length; i++) {
+            System.out.println(objArray[i]);
+        }
+    }
+
+    static class GreekLetter {
+        private String letter;
+        private Integer position;
+
+        public GreekLetter(String letter, Integer position) {
+            this.letter = letter;
+            this.position = position;
+        }
+
+        @Override
+        public String toString() {
+            return "{" +
+                    "letter='" + letter + '\'' +
+                    ", position=" + position +
+                    '}';
+        }
+    }
+}
