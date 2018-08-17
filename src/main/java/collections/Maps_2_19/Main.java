bc1d7f947d1476107ae5a9b52185bc7272e9501b
Vladimir Glinskikh <support@zhelezyaka.kz>
2018-08-17 13:48:02 +0600

    verified

---
+++ src/main/java/collections/Maps_2_19/Main.java
@@ -0,0 +1,15 @@
+package collections.Maps_2_19;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Map map = new TreeMap();
+        map.put("Omega", 24);
+        map.put("Alpha", 1);
+        map.put("Gamma", 3);
+
+        System.out.println(map);
+    }
+}
