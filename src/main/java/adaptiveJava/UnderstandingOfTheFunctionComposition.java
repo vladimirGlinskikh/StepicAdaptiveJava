fdb38bd6d2ee82fbd9dfb7e7cfe4ff475e437a57
VladimirGlinskikh <support@zhelezyaka.kz>
2020-06-10 22:09:17 +0600

    checked out this task

--- src/main/java/adaptiveJava/UnderstandingOfTheFunctionComposition.java
+++ src/main/java/adaptiveJava/UnderstandingOfTheFunctionComposition.java
@@ -1,15 +1,22 @@
 package adaptiveJava;
 
+import java.util.function.Consumer;
 import java.util.function.IntUnaryOperator;
 
 public class UnderstandingOfTheFunctionComposition {
 
     public static void main(String[] args) {
-        IntUnaryOperator mult2 = num -> num * 2;
-        IntUnaryOperator add3 = num -> num + 3;
+//        IntUnaryOperator mult2 = num -> num * 2;
+//        IntUnaryOperator add3 = num -> num + 3;
+//
+//        IntUnaryOperator combinedOperator = add3.compose(mult2.andThen(add3)).andThen(mult2);
+//        int result = combinedOperator.applyAsInt(5);
+//        System.out.println(result);
 
-        IntUnaryOperator combinedOperator = add3.compose(mult2.andThen(add3)).andThen(mult2);
-        int result = combinedOperator.applyAsInt(5);
-        System.out.println(result);
+        Consumer<Integer> printer = System.out::println;
+        Consumer<Integer> devNull = (val) -> {int v = val * 2;};
+
+        Consumer<Integer> combinedConsumer = devNull.andThen(devNull.andThen(printer));
+        combinedConsumer.accept(100);
     }
 }
